216d45b42d4a4a07705a88d905e8928aa55854c8
Nancy Jo <enciie@gmail.com>
2018-06-28 22:41:30 +0000

    intial code for cli greeting

--- lib/best_state_park/cli.rb
+++ lib/best_state_park/cli.rb
@@ -2,16 +2,48 @@ class BestStatePark::CLI
 
   def call
     greeting
+    start
   end
 
-  def greeting
+  def greetingy
     puts "************************************************************"
     puts "*                                                          *"
-    puts "*          Welcome to the Best National Parks CLI          *"
+    puts "*          WELCOME TO THE BEST NATIONAL PARKS CLI          *"
     puts "*                                                          *"
     puts "************************************************************"
     puts ""
-    puts "            Are you ready to go on an adventure?            "
   end
+
+  def start
+    puts ""
+    puts "            Are you ready to go on an adventure?            "
+    puts "                    Please enter Y or N:                    "
+    puts ""
+    input = gets.strip.downcase
+    if input == "y"
+      list_states
+    elsif input == "n"
+      goodbye
+    else
+      puts ""
+      puts "        .......Please enter a valid reponse.......       "
+      puts ""
+      start
+    end
+  end
+
+  def list_states
+    puts ""
+    puts "              Please enter a number to explore:             "
+  end
+
+
+  def goodbye
+    puts ""
+    puts "            GO OUT AND ENJOY THE GREAT OUTDOORS!          "
+    puts "                     HAVE A GREAT DAY!                    "
+    puts ""
+  end
+
 
 end
