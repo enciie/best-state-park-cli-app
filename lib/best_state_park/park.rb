9fb8f4dd7f53a7548296e9c4c6ac1bf19050af5c
Nancy Jo <enciie@gmail.com>
2018-06-29 01:41:40 +0000

    Updated the hardcoded data as attributes

--- lib/best_state_park/park.rb
+++ lib/best_state_park/park.rb
@@ -2,13 +2,7 @@ class BestStatePark::Park
   attr_accessor :state, :park_name, :description
 
   def self.all
-    puts <<-DOC
-    1-Alabama 2-Alaska 3-Arizona 4-Arkansas 5-California
-    6-Colorado 7-Connecticut 8-Delaware 9-Florida 10-Georgia
-    11-Hawaii 12-Idaho 13-Illinois 14-Indiana 15-Iowa
-    16-Kansas 17-Kentucky 18-Louisiana 19-Maine 20-Maryland
-    21-Massachusetts 22-Michigan 23-Minnesota 24-Mississippi 25-Missouri
-    DOC
+    
     park_1 = self.new
     park_1.state = "Alabama"
     park_1.park_name = "national park 1"
@@ -19,7 +13,18 @@ class BestStatePark::Park
     park_2.park_name = "national park 2"
     park_2.description = "info about the national park 2"
 
-    [park_1, park_2]
+    park_3 = self.new
+    park_3.state = "Arizona"
+    park_3.park_name = "national park 3"
+    park_3.description = "info about the national park 3"
+    
+
+    [park_1, park_2, park_3]
+  end
+
+  def self.scrape_page
+    html = https://www.travelandleisure.com/trip-ideas/national-parks/best-national-parks-by-state
+    doc = Nokogiri::HTML(open(html))
   end
 
 end
