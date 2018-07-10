9fa52a50dd0843a1341c7c7be430493ddc5cb9cb
Nancy Jo <enciie@gmail.com>
2018-07-10 05:14:27 +0000

    fixed typos

--- lib/best_state_park/park.rb
+++ lib/best_state_park/park.rb
@@ -3,10 +3,10 @@ class BestStatePark::Park
 
   @@all = []
 
-  def initialize(state=nil, park_name=nil, description=nil)
+  def initialize(state = nil, park_name = nil, description = nil)
     @state = state
     @park_name = park_name
-    @decription = description
+    @description = description
   end
 
   def self.all
@@ -18,8 +18,8 @@ class BestStatePark::Park
   end
 
   def self.find_by_state(state)
-    self.all.detect do |p|
-      p.state.downcase == state.downcase.strip
+    self.all.detect do |park|
+      park.state.downcase == state.downcase.strip
     end
   end
 
